46aaf1dfb0d877b7eb07d69284a08247e79575e3
Nikhil Kumar Tiwari <nikhilcurry456@gmail.com>
2026-05-08 19:32:56 +0530

    more changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const db = require('./lib/DbConnection.js');
+const db = require('./lib/Dbconnection.js');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
